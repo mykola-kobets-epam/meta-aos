78c6f2297fb251f783a2d3f4fd342e91f7300ca9
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-01-05 22:19:44 +0200

    [provfinish] Add sync to flush all changes
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- recipes-aos/aos-provfinish/files/provfinish.sh
+++ recipes-aos/aos-provfinish/files/provfinish.sh
@@ -3,5 +3,10 @@
 # remove unprovisioned flag
 rm -rf /var/aos/.unprovisioned
 
+sync
+
 # restart aos target
-{ sleep 1; systemctl restart aos.target; }&
+{
+    sleep 1
+    systemctl restart aos.target
+} &
