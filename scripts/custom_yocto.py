a5a97314d1127b21bb255a0b61845de7b224cf2a
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-07-25 13:43:52 +0300

    [custom_yocto] Update formatting
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- scripts/custom_yocto.py
+++ scripts/custom_yocto.py
@@ -15,17 +15,22 @@ def call_bitbake(yocto_dir, conf):
         f'bitbake {conf["target"]}',
     ]
     line = " && ".join(c for c in cmd)
+
     ret = os.system(f'bash -c "{line}"')
+
     return ret >> 8
 
 
 def _read_layers_file(enabled_file):
     layers = []
+
     for line in enabled_file:
         line = line.strip()
+
         if len(line) > 0 and line[0] != "#":
             if line not in layers:
                 layers.append(line)
+
     return layers
 
 
@@ -33,6 +38,7 @@ def main():
     """Main function"""
     ret = 0
     parser = argparse.ArgumentParser(description="fota")
+
     parser.add_argument(
         "conf", metavar="conf.yaml", type=str, help="YAML file with configuration"
     )
@@ -43,14 +49,18 @@ def main():
     parser.add_argument(
         "-f", "--file", action="store", help="Config file with enabled layers"
     )
+
     args = parser.parse_args()
 
     with open(args.conf, "r", encoding="utf-8") as conf_file:
         conf = yaml.load(conf_file, Loader=yaml.CLoader)
+
     if args.layers is not None or args.file is not None:
         enabled_layers = []
+
         if args.layers is not None:
             enabled_layers = list(args.layers)
+
         if args.file is not None:
             try:
                 with open(args.file, "r", encoding="utf-8") as enabled_file:
@@ -59,15 +69,19 @@ def main():
                 print(f"File {args.file} can't be opened")
     else:
         enabled_layers = None
+
     layers = conf["layers"]
     yocto_dir = layers["yocto_dir"]
-    for key in layers["items"]:
+
+    for layer in layers["items"]:
         if enabled_layers:
-            enabled = key in enabled_layers
+            enabled = layer in enabled_layers
         else:
-            enabled = layers["items"][key].get("enabled", True)
+            enabled = layers["items"][layer].get("enabled", True)
+
         if enabled:
-            ret = call_bitbake(yocto_dir, layers["items"][key])
+            ret = call_bitbake(yocto_dir, layers["items"][layer])
+
     return ret
 
 
