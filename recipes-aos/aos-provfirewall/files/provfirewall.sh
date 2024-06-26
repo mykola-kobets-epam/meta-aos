96e8b9a322d38ad9817acae8ccee20fb04598ca3
Mykola Solianko <mykola_solianko@epam.com>
2024-06-26 15:55:34 +0300

    [aos-provfirewall] Update firewall script for multinode
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- recipes-aos/aos-provfirewall/files/provfirewall.sh
+++ recipes-aos/aos-provfirewall/files/provfirewall.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
+GATEWAY=$(ip route | grep default | awk '{print $3}' | head -n1)
+
+if [ -z "$GATEWAY" ]; then
+    echo "No default gateway found"
+    exit 1
+fi
+
+apply_rules() {
+    INTERFACE=$1
+    SUBNET=$(ip -o -f inet addr show $INTERFACE | awk '{print $4}' | cut -d'/' -f1-2)
+    
+    if [ -z "$SUBNET" ]; then
+        echo "No IP address found for $INTERFACE"
+        return
+    fi
+
+    for PROTO in tcp udp; do
+        iptables -A INPUT -s $GATEWAY/32 -d $SUBNET -i $INTERFACE -p $PROTO -m $PROTO --dport 8089 -j DROP
+        iptables -A INPUT ! -s $SUBNET -d $SUBNET -i $INTERFACE -p $PROTO -m $PROTO --dport 8089 -j DROP
+        iptables -A INPUT -s $SUBNET -d $SUBNET -i $INTERFACE -p $PROTO -m $PROTO --dport 8089 -j ACCEPT
+    done
+}
+
 find /sys/class/net/*/device | cut -d/ -f5 |
-    while read -r line; do
-        iptables -A INPUT -i "$line" -p tcp --dport 8089 -j DROP
-        iptables -A INPUT -i "$line" -p udp --dport 8089 -j DROP
+    while read -r INTERFACE; do
+        apply_rules "$INTERFACE"
     done
